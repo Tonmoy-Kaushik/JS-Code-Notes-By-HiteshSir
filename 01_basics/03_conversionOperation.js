2d0531e3c0e89c3fb30c9a7e12d62fe5089089b6
Tonmoy-Kaushik <tonmoysarma2@gmail.com>
2024-05-08 03:51:49 +0530

    Added

--- 01_basics/03_conversionOperation.js
+++ 01_basics/03_conversionOperation.js
@@ -1,37 +1,58 @@
-let score = "hitesh"
+//1.
 
-//console.log(typeof score);
-//console.log(typeof(score));
+{let score = "33"
+
+//console.log(typeof score) is string.
+//or can do as a method : console.log(typeof(score)).
 
 let valueInNumber = Number(score)
-//console.log(typeof valueInNumber);
-//console.log(valueInNumber);
+//console.log(typeof valueInNumber) is number.
+//console.log(valueInNumber)is 33 a number.
+}
 
+//2. 
 
-// "33" => 33
-// "33abc" => NaN
-//  true => 1; false => 0
+{let score = "33abc"
 
-let isLoggedIn = "hitesh"
+//console.log(typeof score) is string.
 
-let booleanIsLoggedIn = Boolean(isLoggedIn)
-//  console.log(booleanIsLoggedIn);
+let valueInNumber = Number(score)
+//console.log(typeof valueInNumber) is number, but output of console.log(valueInNumber) is NaN (not a number).
+}
+
+// if we convert Null to number from original type object, it will become 0.  console.log(number(Null)) is 0.
+// console.log(undefined) is undefined and converted to number, it will be NaN (not a number).
+//  Converting boolean to number : true => 1; false => 0
+
+let isLoggedIn = 1;
+let isLoggedIn2 = "String";
+let isLoggedOut = '';
+let x = Boolean(isLoggedIn), y = Boolean(isLoggedIn2), z = Boolean(isLoggedOut) // converting to boolean.
+//  console.log(x) gives true, console.log(y) gives true and console.log(z) gives false as empty string.
 
 // 1 => true; 0 => false
 // "" => false
-// "hitesh" => true
+// "String" => true
+//"" => false.
 
-let someNumber = 33
 
-let stringNumber = String(someNumber)
-// console.log(stringNumber);
-// console.log(typeof stringNumber);
+
+
+
+
+let n = 33
+
+let stringNumber = String(n)
+// console.log(n); is 33 as string.
+// console.log(typeof n); is string.
+
+
 
 // *********************** Operations ***********************
 
 let value = 3
 let negValue = -value
-// console.log(negValue);
+// console.log(negValue); is -3
 
 // console.log(2+2);
 // console.log(2-2);
@@ -44,25 +65,24 @@ let str1 = "hello"
 let str2 = " hitesh"
 
 let str3 = str1 + str2
-// console.log(str3);
+// console.log(str3); concatenation that outputs hello hitesh
 
-// console.log("1" + 2);
-// console.log(1 + "2");
-// console.log("1" + 2 + 2);
-// console.log(1 + 2 + "2");
+// console.log("1" + 2); is 12
+// console.log(1 + "2"); is 12
+// console.log("1" + 2 + 2); is 122 bcz first ype is string.
+// console.log(1 + 2 + "2"); is 32 bcz first type is number.
 
-// console.log( (3 + 4) * 5 % 3);
+// console.log( (3 + 4) * 5 % 3); a good practice to keep 
 
-// console.log(+true);
-// console.log(+"");
+// console.log(+true); true becomes number 1 and it gets incremented to 2. true+ direct display is not possible as incase of x+ the increment operator updates the value in next use of that variable, called postfix.
+// console.log(+""); "" is false or 0 and gets incremented to 1.
 
-let num1, num2, num3
-
-num1 = num2 = num3 = 2 + 2
+let num1, num2, num3 // we can assume multiple  variables togather.
 
 let gameCounter = 100
-++gameCounter;
-console.log(gameCounter);
+++gameCounter; // prefix 
+gameCounter; //postfix
+console.log(gameCounter); // is 101.
 
 // link to study
 // https://tc39.es/ecma262/multipage/abstract-operations.html#sec-type-conversion
