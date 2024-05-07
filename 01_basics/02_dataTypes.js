d5c841e95e280f5548ee10dae8a6e840fa846a06
Tonmoy-Kaushik <tonmoysarma2@gmail.com>
2024-05-07 13:48:03 +0530

    Added

--- 01_basics/02_dataTypes.js
+++ 01_basics/02_dataTypes.js
@@ -1,29 +1,31 @@
-"use strict"; // treat all JS code as newer version
+"use strict"; // treat all JS codes as newer version syntax of ECMAScript.
 
-// alert( 3 + 3) // we are using nodejs, not browser
+// 7 Data Types :
 
-console.log(3 
-    +
-     3) // code readability should be high
-
-console.log("Hitesh")
-
+// number => - to + of 2 to power 53
+// bigint
+// string => ""
+// boolean => true/false
+// null => standalone value, that comes when we set value of a variable/fn as 'null' due to explicit absence of value for a pre existing variable. eg. let f = null;console.log(f); Output: null and typeof it is object.
+// undefined => when there is no value assigned to a variable/ function has no value to log, it is undefined.
+// symbol => unique values. 
 
 let name = "hitesh"
 let age = 18
 let isLoggedIn = false
 let state;
 
-// number => 2 to power 53
-// bigint
-// string => ""
-// boolean => true/false
-// null => standalone value
-// undefined => 
-// symbol => unique
-
-
-// object
 
 console.log(typeof undefined); // undefined
 console.log(typeof null); // object
+
+
+Extras :
+// alert( 3 + 3) // we are using nodejs, not browser, so it can't be popped up.
+// Prompt can still be used by using a special npm prompt-sync.
+
+console.log(3 
+    +
+     3) // bad practice as code readability should be high.
+
+     console.log(3 + 3); console.log("Hitesh") // bad practice as bad readability.
