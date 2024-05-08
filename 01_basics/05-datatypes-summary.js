44e0db201e9682134d2c254d3341eaf0c80847e6
Tonmoy-Kaushik <tonmoysarma2@gmail.com>
2024-05-09 04:21:37 +0530

    ok

--- 01_basics/05-datatypes-summary.js
+++ 01_basics/05-datatypes-summary.js
@@ -44,4 +44,4 @@ console.log(typeof anotherId);  // way to check datatype.
 
 // Stack memory assigns the copy of a variable to another variable. Eg. let num = 5; let dig = num; dig = 2; displaying num will give 5 and dig will give 2. Here a copy of num is assigned to dig, so by updating dig (which was equal to num's copy) will not effect num.  So when two variable are equal to each other in primitive type, it is just the copy of the 1st variable that is equal to the new one.
 
-//In heap, the real value is assigned. eg. let user1 = { name : 'John', age : 24}; let user2 = user1; user2.age = 21; here displaying user2's age will give 21 but user1 will also give the same as it will stay equal to user2 all the time. So changing one will have effect on both objects.
+//In Heap Memory, the real value is assigned. eg. let user1 = { name : 'John', age : 24}; let user2 = user1; user2.age = 21; here displaying user2's age will give 21 but user1 will also give the same as it will stay equal to user2 all the time. So changing one will have effect on both objects.
