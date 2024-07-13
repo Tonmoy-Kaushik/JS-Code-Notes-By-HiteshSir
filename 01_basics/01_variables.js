325a5a0359073a5183ab4810d97ccd862ddaf52d
Tonmoy-Kaushik <tonmoysarma2@gmail.com>
2024-07-13 15:41:38 +0530

    Added

--- 01_basics/01_variables.js
+++ 01_basics/01_variables.js
@@ -1,6 +1,6 @@
-const Id = 144     // Can't be changed as const.
+const Id = 144     // Can't be changed as it's const.
 
-let Email = "hitesh@google.com"      // Can be changed but the variable is limited to scope of a fn block{}, so same variable can be used again in other blocks with different values without effecting each other.
+let Email = "hitesh@google.com"      // it can be changed but the variable is limited to scope of a fn block{}, so same variable can be used again in other blocks with different values without effecting each other.
 
 var Password = "12345"   // Can be changed all the time everywhere as universal scoping, so we can accidentally reuse the variable in later scripts in the same code file.
 
@@ -14,11 +14,11 @@ Email = "hc@hc.com"
 Password = "21212121"
 City = "Bengaluru"
 
-console.log(Id);
+console.log(Id); // 144
 
 /*
 Prefer not to use var because of issue in block scope and functional scope
 */
 
 
-console.table([Id, Email, Password, City, State])
+console.table([Id, Email, Password, City, State])   // creates a table of index nd values.
