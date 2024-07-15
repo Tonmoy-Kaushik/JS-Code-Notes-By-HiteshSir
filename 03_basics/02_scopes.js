f063a045a67dbf9132f404c63f5264167e396370
Tonmoy-Kaushik <tonmoysarma2@gmail.com>
2024-07-15 23:11:14 +0530

    h

--- 03_basics/02_scopes.js
+++ 03_basics/02_scopes.js
@@ -1,17 +1,23 @@
-//var c = 300
-let a = 300
+let a = 100;
+const b = 200;
+var c = 300
 if (true) {
     let a = 10
     const b = 20
-    // console.log("INNER: ", a);
+    var c = 30
+    
+    console.log("INNER: a is ", a); 
+    console.log("INNER: b is ", b);
+    console.log("INNER: c is ", c);
     
 }
 
+ console.log("OUTER: a is ", a);
+ console.log("OUTER: b is ", b);
+ console.log("OUTER: c is ", c);
 
 
-// console.log(a);
-// console.log(b);
-// console.log(c);
+
 
 
 function one(){
@@ -19,40 +25,45 @@ function one(){
 
     function two(){
         const website = "youtube"
-        console.log(username);
+        console.log(username); // will work whenever called as global values are accessible inside children block.
     }
-    // console.log(website);
+    console.log(website); //  won't work since block scope values are not accessible in global scope. 
 
      two()
 
 }
+// one() the whole fn will be executed in this point.
+/* we always need to execute the top level parent function in order to run all the children function nested inside it in different levels. */
 
-// one()
 
 if (true) {
     const username = "hitesh"
     if (username === "hitesh") {
         const website = " youtube"
-        // console.log(username + website);
+        console.log(username + website);
     }
-    // console.log(website);
+    // console.log(website); won't work 
 }
 
-// console.log(username);
+// console.log(username); won't work
 
 
-// ++++++++++++++++++ interesting ++++++++++++++++++
 
 
-console.log(addone(5))
+
+ // ++++++++++++++++++ interesting ++++++++++++++++++
+
+// We can declare a function in normal way like here : 
+
+console.log(addone(5)) // In normal declaration, we can use the function even before declaring.
 
 function addone(num){
-    return num + 1
+    return num + 1 
 }
 
 
-
-addTwo(5)
+// We can also declare function by holding it in a variable, which is called an expression this way, but we cant use it before declaration.
+addTwo(5) // error.
 const addTwo = function(num){
     return num + 2
 }
