02179ae80de69241fc5bbd6236ca6bd08f961739
Tonmoy-Kaushik <tonmoysarma2@gmail.com>
2024-07-18 10:52:46 +0530

    j

--- 05_iterations/04_for_in_loop.js
+++ 05_iterations/04_for_in_loop.js
@@ -1,3 +1,5 @@
+// For in loop returns keys/index of a obj/array.
+
 const myObject = {
     js: 'javascript',
     cpp: 'C++',
@@ -6,14 +8,14 @@ const myObject = {
 }
 
 for (const key in myObject) {
-    //console.log(`${key} shortcut is for ${myObject[key]}`);
-}
+    console.log(`${key} shortcut is for ${myObject[key]}`);
+} // using for in loop on obj. We can get the keys with help of for in loop, and once the keys are available, we use dot/square brkt. notation with the obj. to get values.
 
 const programming = ["js", "rb", "py", "java", "cpp"]
 
 for (const key in programming) {
     //console.log(programming[key]);
-}
+} //prints index no.s
 
 // const map = new Map()
 // map.set('IN', "India")
@@ -23,4 +25,4 @@ for (const key in programming) {
 
 // for (const key in map) {
 //     console.log(key);
-// }
+// } not applicable on maps, explain from net.
