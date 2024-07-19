63b55a6e24004de79527f8abf0f42ef2fffdeffd
Tonmoy-Kaushik <168622801+Tonmoy-Kaushik@users.noreply.github.com>
2024-07-19 15:21:21 +0530

    Update 08_.js

--- 05_iterations/08_.js
+++ 05_iterations/08_.js
@@ -1,11 +1,16 @@
-const myNums = [1, 2, 3]
+// With help of reduce,we can add/mulitply etc. all the elements inside an array in a chain. It just takes a initial value next to end of fn, 0 here, and inside fn, we need to use 1st variable to represent sum/result in each step, and a 2nd variable that represents current element; then we added both here for returning a final result. 
 
-// const myTotal = myNums.reduce(function (acc, currval) {
-//     console.log(`acc: ${acc} and currval: ${currval}`);
-//     return acc + currval
+const Arr = [1, 2, 3]
+
+// const ArrTotal = Arr.reduce(function (sum, element) {
+//     console.log(`sum: ${sum} and element: ${element}`);  to print steps of addition inside.
+//     return sum + elemnt
 // }, 0)
 
-const myTotal = myNums.reduce( (acc, curr) => acc+curr, 0)
+
+
+// shortest way to use : 
+const ArrTotal = Arr.reduce( (sum,element) => sum + element, 0)
 
 console.log(myTotal);
 
@@ -29,6 +34,6 @@ const shoppingCart = [
     },
 ]
 
-const priceToPay = shoppingCart.reduce((acc, item) => acc + item.price, 0)
+const priceToPay = shoppingCart.reduce((sum,element) => sum + element, 0)
 
 console.log(priceToPay);
